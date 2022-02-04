5446e56fe68ac318d540683d94f700a4806f4970
Samih Ouague <ouaguenouni.samih@gmail.com>
2022-02-04 04:22:10 +0100

    API Config modif

--- lib/Config.php
+++ lib/Config.php
@@ -2,9 +2,9 @@
 class Config {
     public static function getConfig() {
         $config = [
-            "passphrase" => "1mysf361tzq",
-            "key" => "bfada5e5e8fe74dab2f3154d7b890cd7",
-            "secret" => "kADf6WCZ6aGldK8wsOisfCdE1yV315cXY6sB7MjrCe2q6MxCIsGJhJN3ajiozasQkhzk7pksEykoCWrvpgsunA=="
+            "passphrase" => "API Passphrase",
+            "key" => "API Key",
+            "secret" => "API secret Key"
         ];
         return $config;
     }
